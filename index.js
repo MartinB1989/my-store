81c1dceed777c12ebb6ed54ef52ed6dd2ee91253
MartiB1989 <j.martin.b1989@gmail.com>
2022-07-07 21:07:36 -0300

    deploy

--- index.js
+++ index.js
@@ -2,22 +2,34 @@ const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routes');
 
-const {logErrors, errorHandler} = require('./middlewares/error.handler')
+const {logErrors, errorHandler} = require('./middlewares/error.handler');
+const { options } = require('./routes/products.router');
 
 const app = express()
-const port = 5500
+const port = process.env.PORT || 3000
 
+
+const whiteList = ['http://127.0.0.1:5500','http://myapp.com', 'http://dos.es']
+const corsOptions = {
+  origin: (origin, callback) => {
+    if(whiteList.includes(origin) || !origin){
+      callback(null,true)
+    } else {
+      callback(new Error('No permitido'), false)
+    }
+  }
+}
+app.use(cors(corsOptions));
 app.use(express.json()) //Para poder enviar datos en formato json a través del metodo post
-app.use(cors())
 // app.use(function (req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   next()
-// })
+  //   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  //   next()
+  // })
 routerApi(app)
 
 app.use(logErrors)
 app.use(errorHandler)
 
 app.listen(port, () => {
-  console.log('Mi port' + port)
+  console.log('Desarrollando: ' + port)
 })
